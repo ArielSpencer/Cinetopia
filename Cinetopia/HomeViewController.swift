51a678663a14600f41824f67bd7d87bb678df058
Ariel Spencer <arielspencer.tech@gmail.com>
2024-12-06 23:06:22 -0300

    refactor: organize code with private lazy label and encapsulated setup functions

--- Cinetopia/HomeViewController.swift
+++ Cinetopia/HomeViewController.swift
@@ -8,25 +8,36 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        view.backgroundColor = UIColor.background
-        
+    
+    private lazy var label: UILabel = {
         let label = UILabel()
+        
         label.translatesAutoresizingMaskIntoConstraints = false
         label.text = "Hello World!"
         label.textColor = .white
         label.font = .systemFont(ofSize: 24, weight: .bold)
         
-        view.addSubview(label)
+        return label
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = UIColor.background
+        addSubview()
+        setupConstraints()
         
+    }
+    
+    private func addSubview() {
+        view.addSubview(label)
+    }
+    
+    private func setupConstraints() {
         NSLayoutConstraint.activate([
             label.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -16),
             label.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 32)
         ])
-        
     }
-
+    
 }
