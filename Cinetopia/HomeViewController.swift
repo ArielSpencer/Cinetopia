709432beaf101a727efe88f5f82b03cc78364cc0
Ariel Spencer <arielspencer.tech@gmail.com>
2024-12-07 23:02:22 -0300

    feat: add couple to HomeViewController

--- Cinetopia/HomeViewController.swift
+++ Cinetopia/HomeViewController.swift
@@ -12,7 +12,12 @@ class HomeViewController: UIViewController {
     private lazy var logoImageView: UIImageView = {
         let imageView = UIImageView(image: UIImage.logo)
         imageView.translatesAutoresizingMaskIntoConstraints = false
-        
+        return imageView
+    }()
+    
+    private lazy var coupleImageView: UIImageView = {
+        let imageView = UIImageView(image: .couple)
+        imageView.translatesAutoresizingMaskIntoConstraints = false
         return imageView
     }()
 
@@ -27,12 +32,16 @@ class HomeViewController: UIViewController {
     
     private func addSubview() {
         view.addSubview(logoImageView)
+        view.addSubview(coupleImageView)
     }
     
     private func setupConstraints() {
         NSLayoutConstraint.activate([
             logoImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 64),
-            logoImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+            logoImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            
+            coupleImageView.topAnchor.constraint(equalTo: logoImageView.bottomAnchor, constant: 32),
+            coupleImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor)
         ])
     }
     
