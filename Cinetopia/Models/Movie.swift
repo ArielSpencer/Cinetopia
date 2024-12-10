8606c760f766760d14ff86b125058a6038f1f494
Ariel Spencer <arielspencer.tech@gmail.com>
2024-12-09 23:03:50 -0300

    feat: add Movie struct to represent movies

---
+++ Cinetopia/Models/Movie.swift
@@ -0,0 +1,17 @@
+//
+//  Movie.swift
+//  Cinetopia
+//
+//  Created by Ariel Spencer on 09/12/24.
+//
+
+import Foundation
+
+struct Movie {
+    let id: Int
+    let title: String
+    let image: String
+    let synopsis: String
+    let rate: String
+    let releaseDate: String
+}
