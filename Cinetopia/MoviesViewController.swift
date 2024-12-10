11eac4d82a83d08ae30fbb65c4fb611a734d5145
Ariel Spencer <arielspencer.tech@gmail.com>
2024-12-09 22:35:09 -0300

    feat: add TableView for movies list

--- Cinetopia/MoviesViewController.swift
+++ Cinetopia/MoviesViewController.swift
@@ -8,12 +8,34 @@
 import UIKit
 
 class MoviesViewController: UIViewController {
+    
+    private lazy var tableView: UITableView = {
+       let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.backgroundColor = .clear
+        return tableView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .background
         setupNavigationBar()
+        addSubViews()
+        setupConstraints()
         // Do any additional setup after loading the view.
+    }
+    
+    private func addSubViews() {
+        view.addSubview(tableView)
+    }
+    
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
     }
     
     private func setupNavigationBar() {
