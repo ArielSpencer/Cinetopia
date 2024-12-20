5ba6c14c123be80b08356f1c08ed43846c1da3e5
Ariel Spencer <arielspencer.tech@gmail.com>
2024-12-19 22:18:04 -0300

    feat: add TabBarController to manage navigation between popular movies and favorites

---
+++ Cinetopia/ViewControllers/TabBarController.swift
@@ -0,0 +1,37 @@
+//
+//  TabBarController.swift
+//  Cinetopia
+//
+//  Created by Ariel Spencer on 19/12/24.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+    
+    // MARK: - View life cycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupTabBarController()
+    }
+    
+    // MARK: - Class methods
+    
+    private func setupTabBarController() {
+        let home = UINavigationController(rootViewController: MoviesViewController())
+        let symbolConfiguration = UIImage.SymbolConfiguration(scale: .medium)
+        
+        let homeSymbol = UIImage(systemName: "film", withConfiguration: symbolConfiguration)
+        home.tabBarItem.image = homeSymbol
+        home.tabBarItem.title = "Filmes populares"
+        
+        let favorites = UINavigationController(rootViewController: FavoriteMoviesViewController())
+        
+        let heartSymbol = UIImage(systemName: "heart", withConfiguration: symbolConfiguration)
+        favorites.tabBarItem.image = heartSymbol
+        favorites.tabBarItem.title = "Favoritos"
+        
+        self.setViewControllers([home, favorites], animated: true)
+    }
+}
