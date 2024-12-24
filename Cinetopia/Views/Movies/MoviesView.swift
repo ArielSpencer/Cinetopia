705207329064eb0c37adcae17585a5975619df3e
Ariel Spencer <arielspencer.tech@gmail.com>
2024-12-23 22:42:49 -0300

    refactor: separate view from ViewController to initiate MVP structure

---
+++ Cinetopia/Views/Movies/MoviesView.swift
@@ -0,0 +1,94 @@
+//
+//  MoviesView.swift
+//  Cinetopia
+//
+//  Created by Ariel Spencer on 23/12/24.
+//
+
+import UIKit
+
+class MoviesView: UIView {
+    
+    // MARK: - Attributes
+    
+    private var filteredMovies: [Movie] = []
+    private var isSearchActive: Bool = false
+    private var movies: [Movie] = []
+
+    // MARK: - UI Components
+    
+    private lazy var tableView: UITableView = {
+       let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.backgroundColor = .clear
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.register(MovieTableViewCell.self, forCellReuseIdentifier: "movieCell")
+        return tableView
+    }()
+    
+    private lazy var searchBar: UISearchBar = {
+       let searchBar = UISearchBar()
+        searchBar.translatesAutoresizingMaskIntoConstraints = false
+        searchBar.placeholder = "Pesquisar"
+        searchBar.searchTextField.backgroundColor = .white
+        searchBar.delegate = self
+        return searchBar
+    }()
+    
+    // MARK: - Init
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Class methods
+    
+    private func addSubViews() {
+        addSubview(tableView)
+    }
+    
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: bottomAnchor)
+        ])
+    }
+    
+}
+
+extension MoviesView: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return isSearchActive ? filteredMovies.count : movies.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "movieCell", for: indexPath) as? MovieTableViewCell {
+            let movie = isSearchActive ? filteredMovies[indexPath.row] : movies[indexPath.row]
+            cell.configureCell(movie: movie)
+            cell.delegate = self
+            cell.selectionStyle = .none
+            return cell
+        }
+        
+        return UITableViewCell()
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        let movie = isSearchActive ? filteredMovies[indexPath.row] : movies[indexPath.row]
+        let detailsVC = MovieDetailsViewController(movie: movie)
+        navigationController?.pushViewController(detailsVC, animated: true)
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 160
+    }
+}
--- Cinetopia/Views/Movies/MoviesViewController.swift
+++ Cinetopia/Views/Movies/MoviesViewController.swift
@@ -9,30 +9,7 @@ import UIKit
 
 class MoviesViewController: UIViewController {
     
-    private var filteredMovies: [Movie] = []
-    private var isSearchActive: Bool = false
-    
     private let movieService: MovieService = MovieService()
-    private var movies: [Movie] = []
-    
-    private lazy var tableView: UITableView = {
-       let tableView = UITableView()
-        tableView.translatesAutoresizingMaskIntoConstraints = false
-        tableView.backgroundColor = .clear
-        tableView.dataSource = self
-        tableView.delegate = self
-        tableView.register(MovieTableViewCell.self, forCellReuseIdentifier: "movieCell")
-        return tableView
-    }()
-    
-    private lazy var searchBar: UISearchBar = {
-       let searchBar = UISearchBar()
-        searchBar.translatesAutoresizingMaskIntoConstraints = false
-        searchBar.placeholder = "Pesquisar"
-        searchBar.searchTextField.backgroundColor = .white
-        searchBar.delegate = self
-        return searchBar
-    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -67,19 +44,6 @@ class MoviesViewController: UIViewController {
         }
     }
     
-    private func addSubViews() {
-        view.addSubview(tableView)
-    }
-    
-    private func setupConstraints() {
-        NSLayoutConstraint.activate([
-            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
-        ])
-    }
-    
     private func setupNavigationBar() {
         title = "Filmes populares"
         navigationController?.navigationBar.prefersLargeTitles = true
@@ -98,35 +62,6 @@ class MoviesViewController: UIViewController {
     }
     */
 
-}
-
-extension MoviesViewController: UITableViewDataSource, UITableViewDelegate {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return isSearchActive ? filteredMovies.count : movies.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        if let cell = tableView.dequeueReusableCell(withIdentifier: "movieCell", for: indexPath) as? MovieTableViewCell {
-            let movie = isSearchActive ? filteredMovies[indexPath.row] : movies[indexPath.row]
-            cell.configureCell(movie: movie)
-            cell.delegate = self
-            cell.selectionStyle = .none
-            return cell
-        }
-        
-        return UITableViewCell()
-    }
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        tableView.deselectRow(at: indexPath, animated: true)
-        let movie = isSearchActive ? filteredMovies[indexPath.row] : movies[indexPath.row]
-        let detailsVC = MovieDetailsViewController(movie: movie)
-        navigationController?.pushViewController(detailsVC, animated: true)
-    }
-    
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 160
-    }
 }
 
 extension MoviesViewController: UISearchBarDelegate {
